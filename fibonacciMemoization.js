79da2019fb4e57f3880730302e09694dd0dfa47b
grellheist <grellheist@tutanota.com>
2023-03-17 21:14:51 -0300

    Updated

---
+++ fibonacciMemoization.js
@@ -0,0 +1,17 @@
+// Classic recursive function - O(2^n)
+
+//const fib = (n) => {
+//    if (n <= 2) return 1;
+//    return fib(n - 1) + fib (n - 2);
+//}
+
+
+// Memoization - O(n)
+
+const fib = (n, memo = {}) => {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    return memo[n];
+}
+
