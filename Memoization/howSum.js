70fc356ac7bea2e1dc9d3d5184342ab6c86de547
grellheist <grellheist@tutanota.com>
2023-03-26 12:31:34 -0300

    Updated

--- Memoization/howSum.js
+++ Memoization/howSum.js
@@ -1,21 +1,38 @@
 // Recursive implementation
-const howSum = (targetSum, numbers) => {
-  if (targetSum === 0) return [];
-  if (targetSum < 0) return null;
+//const howSum = (targetSum, numbers) => {
+//    if (targetSum === 0) return [];
+//    if (targetSum < 0) return null;
+//
+//    for (const number of numbers) {
+//        const remainder = targetSum - number;
+//        const remainderResult = howSum(remainder, numbers);
+//        if (remainderResult !== null) {
+//            return [...remainderResult, number];
+//        }
+//    }
+//    return null;
+//};
 
-  for (let number of numbers) {
-    const remainder = targetSum - number;
-    const remainderResult = howSum(remainder, numbers);
-    if (remainderResult !== null) {
-      return [...remainderResult, number];
+// Memoized implementation
+const howSum = (targetSum, numbers, memoObject = {}) => {
+    if (targetSum in memoObject) return memoObject[targetSum];
+    if (targetSum === 0) return [];
+    if (targetSum < 0) return null;
+
+    for (const number of numbers) {
+        const remainder = targetSum - number;
+        const remainderResult = howSum(remainder, numbers, memoObject);
+        if (remainderResult !== null) {
+            memoObject[targetSum] = [...remainderResult, number];
+            return memoObject[targetSum];
+        }
     }
-  }
-
-  return null;
+    memoObject[targetSum] = null;
+    return null;
 };
 
 console.log(howSum(7, [2, 3])); // [3, 2, 2]
 console.log(howSum(7, [5, 3, 4, 7])); // [4, 3]
 console.log(howSum(7, [2, 4])); // null
-console.log(howSum(8, [3, 5])); // [2, 2, 2, 2]
+console.log(howSum(8, [2, 3, 5])); // [2, 2, 2, 2]
 console.log(howSum(300, [7, 14])); // null
