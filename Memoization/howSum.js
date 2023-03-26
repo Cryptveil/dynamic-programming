72f0b80cbdbbf70497372e87f70bd5789f742aff
grellheist <grellheist@tutanota.com>
2023-03-26 19:16:03 -0300

    Updated

--- Memoization/howSum.js
+++ Memoization/howSum.js
@@ -31,6 +31,7 @@ const howSum = (targetSum, numbers, memoObject = {}) => {
     return null;
 };
 
+// Test cases
 console.log(howSum(7, [2, 3])); // [3, 2, 2]
 console.log(howSum(7, [5, 3, 4, 7])); // [4, 3]
 console.log(howSum(7, [2, 4])); // null
